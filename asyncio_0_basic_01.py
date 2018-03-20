c4a04a7640574f93f221f63edb8ff6305aa69211
Alberto Vara <a.vara.1986@gmail.com>
2018-03-20 22:16:23 +0100

    Updated asyncio slides

--- asyncio_0_basic_01.py
+++ asyncio_0_basic_01.py
@@ -12,5 +12,7 @@ async def print_sum(x, y):
     print("%s + %s = %s" % (x, y, result))
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.wait([print_sum(i, i+1) for i in range(10)]))
+coroutine = [print_sum(i, i+1) for i in range(10)]
+print(coroutine)
+loop.run_until_complete(asyncio.wait(coroutine))
 loop.close()
