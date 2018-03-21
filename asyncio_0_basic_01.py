14f623b6ffbb87eb68b01f161707be934214d4d7
Alberto Vara <a.vara.1986@gmail.com>
2018-03-21 19:49:53 +0100

    Updated example

--- asyncio_0_basic_01.py
+++ asyncio_0_basic_01.py
@@ -4,15 +4,18 @@ import random
 
 async def compute(x, y):
     print("Compute %s + %s ..." % (x, y))
-    await asyncio.sleep(random.randint(1,20))
+    await asyncio.sleep(random.randint(1, 2))
     return x + y
+
 
 async def print_sum(x, y):
     result = await compute(x, y)
     print("%s + %s = %s" % (x, y, result))
 
+
 loop = asyncio.get_event_loop()
-coroutine = [print_sum(i, i+1) for i in range(10)]
+coroutine = [print_sum(i, i + 1) for i in range(10)]
 print(coroutine)
-loop.run_until_complete(asyncio.wait(coroutine))
+result = loop.run_until_complete(asyncio.wait(coroutine))
+print(result)
 loop.close()
